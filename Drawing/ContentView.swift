0faab4233b2b5f572a80fb6b78f5c5afd8ba15d2
midori2012 <misono5861@gmail.com>
2019-12-17 10:42:32 +0900

    init base

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by misono on 2019/12/16.
+//  Copyright © 2019 misono. All rights reserved.
+//  Text: https://www.hackingwithswift.com/100/swiftui/43
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
