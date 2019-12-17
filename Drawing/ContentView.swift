7219e95945a5982efc4a462b207e3fb3a206cd50
midori2012 <misono5861@gmail.com>
2019-12-17 21:57:18 +0900

    Animating simple shapes with animatableData

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,32 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var insetAmount: CGFloat = 50
+
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                /**
+                 Every time you tap the trapezoid, insetAmount gets set to a new value, causing the shape to be redrawn.
+
+                 Wouldn’t it be nice if we could animate the change in inset? Sure it would – try changing the onTapGesture() closure
+                 
+                - Note: Now run it again, and… nothing has changed. We’ve asked for animation, but we aren’t getting animation – what gives?
+
+                When looking at animations previously, I asked you to add a call to print() inside the body property, then said this:
+
+                ”What you should see is that it prints out 2.0, 3.0, 4.0, and so on. At the same time, the button is scaling up or down smoothly – it doesn’t just jump straight to scale 2, 3, and 4. What’s actually happening here is that SwiftUI is examining the state of our view before the binding changes, examining the target state of our views after the binding changes, then applying an animation to get from point A to point B.”
+
+                So, as soon as self.insetAmount is set to a new random value, it will immediately jump to that value and pass it directly into Trapezoid – it won’t pass in lots of intermediate values as the animation happens. This is why our trapezoid jumps from inset to inset; it has no idea an animation is even happening.
+                 
+                 - Attention: We can fix this in only four lines of code, one of which is just a closing brace.
+                    However, even though this code is simple, the way it works might bend your brain.
+                 
+                 ## when we use withAnimation()
+                 SwiftUI immediately changes our state property to its new value, but behind the scenes it’s also keeping track of the changing value over time as part of the animation. As the animation progresses, **SwiftUI will set the animatableData property of our shape to the latest value**, and it’s down to us to decide what that means – in our case we assign it directly to insetAmount, because that’s the thing we want to animate.
+                 */
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+//                self.insetAmount = CGFloat.random(in: 10...90)
+            }
+         
+    }
+    
+    
     /**
      There are lots of other blend modes to choose from, and it’s worth spending some time experimenting to see how they work. Another popular effect is called screen, which does the opposite of multiply: it inverts the colors, performs a multiply, then inverts them again, resulting in a brighter image rather than a darker image.
      
      As an example, we could render three circles at various positions inside a Stack, then use a slider to control their size and overlap
      */
-    @State private var amount: CGFloat = 0.0
-    var body: some View {
-        VStack {
-            ZStack {
-                /**
-                 There are a host of other real-time effects we can apply, and we already looked at blur() back in project 3. So, let’s look at just one more before we move on: saturation(), which adjusts how much color is used inside a view. Give this a value between 0 (no color, just grayscale) and 1 (full color).
-
-                 We could write a little code to demonstrate both blur() and saturation() in the same view
-                 */
-                Image("Henri")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 300)
-                .saturation(Double(amount))
-                .blur(radius: (1 - amount) * 20)
-            }.frame(width: 300, height: 300)
-            Slider(value: $amount).padding()
-        }.frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
-    }
+//    @State private var amount: CGFloat = 0.0
+//    var body: some View {
+//        VStack {
+//            ZStack {
+//                /**
+//                 There are a host of other real-time effects we can apply, and we already looked at blur() back in project 3. So, let’s look at just one more before we move on: saturation(), which adjusts how much color is used inside a view. Give this a value between 0 (no color, just grayscale) and 1 (full color).
+//
+//                 We could write a little code to demonstrate both blur() and saturation() in the same view
+//                 */
+//                Image("Henri")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 300, height: 300)
+//                .saturation(Double(amount))
+//                .blur(radius: (1 - amount) * 20)
+//            }.frame(width: 300, height: 300)
+//            Slider(value: $amount).padding()
+//        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(Color.black)
+//        .edgesIgnoringSafeArea(.all)
+//    }
 //    var body: some View {
 //        VStack {
 //            ZStack {
@@ -94,4 +129,31 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Trapezoid: Shape {
+    /// - Note: First, the code – add this new computed property to the Trapezoid struct now
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set {
+            /**
+              - Important:
+                Remember, SwiftUI evaluates our view state before an animation was applied and then again after. It can see we originally had code that evaluated to Trapezoid(insetAmount: 50), but then after a random number was chosen we ended up with (for example) Trapezoid(insetAmount: 62). So, it will interpolate between 50 and 62 over the length of our animation, each time setting the animatableData property of our shape to be that latest interpolated value – 51, 52, 53, and so on, until 62 is reached.
+             */
+//            print("insetAmount\(newValue)")
+            self.insetAmount = newValue }
+    }
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
 }
